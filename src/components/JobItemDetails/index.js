d40793b5dfe9bfd16e644b2a6951b31d585940aa
anil-app-cloud <anilkumarbaatula@gmail.com>
2024-11-21 17:05:35 +0000

    first commit

--- src/components/JobItemDetails/index.js
+++ src/components/JobItemDetails/index.js
@@ -76,13 +76,14 @@ class JobItemDetails extends Component {
     const response = await fetch(url, options)
     if (response.ok === true) {
       const data = await response.json()
-      console.log(data)
+      // console.log(data)
       const updatedData = this.getFormattedData(data.job_details)
+      // console.log(updatedData)
       const updatedSimilarJobsData = data.similar_jobs.map(eachSimilarJob =>
         this.getFormattedSimilarData(eachSimilarJob),
       )
-      console.log(updatedData)
-      console.log(updatedSimilarJobsData)
+      // console.log(updatedData)
+      // console.log(updatedSimilarJobsData)
       this.setState({
         jobData: updatedData,
         similarJobsData: updatedSimilarJobsData,
@@ -114,6 +115,7 @@ class JobItemDetails extends Component {
 
         <button
           type="button"
+          data-testid="button"
           className="job-item-failure-button"
           onClick={this.getJobData}
         >
